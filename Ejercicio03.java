f6d1b91fb20a93d0d805add13700800a368d76bc
pabloGm1204 <pabloGm1204@gmail.com>
2023-01-23 20:45:03 +0100

    Ejercicio3

---
+++ Ejercicio03.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ejercicio03{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> a = new ArrayList<Integer>(10);
+        int n = 0;
+
+        for(int i = 0; i<10; i++){
+            System.out.print("Dime el "+(i+1)+" número: ");
+            n = sc.nextInt();
+            a.add(n);
+        }
+
+        System.out.println("El array sin ordenar:");
+        for (Integer i : a) {
+            System.out.println(i);
+        }
+
+        //Ordenar los números
+        Collections.sort(a);
+
+        //Muestra el array
+        System.out.println("El array ordenado:");
+        for (Integer i : a) {
+            System.out.println(i);
+        }
+    }
+}
